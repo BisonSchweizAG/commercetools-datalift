6dacdc716b1ab4354769e7fc7d61a6e86addf2af
Patrick Zbinden <patrick.zbinden@bison-group.com>
2024-06-27 13:05:24 +0200

    Wow, first green test ;-)

--- src/test/java/tech/bison/datalift/DataliftTest.java
+++ src/test/java/tech/bison/datalift/DataliftTest.java
@@ -1,12 +1,24 @@
 package tech.bison.datalift;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class DataliftTest {
+@ExtendWith(MockitoExtension.class)
+class DataliftTest {
+
+  @Mock
+  private Versioner versioner;
+  @Mock
+  private ScriptLoader scriptLoader;
+  @Mock
+  private Runner runner;
+
   @Test
   void bla() {
     Context context = null;
-    DataLift dataLift = new DataLift(null, null, null);
+    DataLift dataLift = new DataLift(versioner, scriptLoader, runner);
     dataLift.execute(context);
   }
 }
