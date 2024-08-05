7fea33b92889ff8d19045e8e6998884c112df057
studix <marco.studer@gmx.net>
2024-08-05 18:29:23 +0200

    replace license header

--- datalift-core/src/main/java/tech/bison/datalift/core/DataMigration.java
+++ datalift-core/src/main/java/tech/bison/datalift/core/DataMigration.java
@@ -2,7 +2,7 @@
  * ========================LICENSE_START=================================
  * datalift
  * ========================================================================
- * Copyright (C) 2010 - 2024 Red Gate Software Ltd
+ * Copyright (C) 2000 - 2024 Bison Schweiz AG
  * ========================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,17 +24,15 @@ import com.commercetools.api.client.ProjectApiRoot;
 /**
  * Interface for Java-based Migrations.
  *
- *  <p>Migration classes implementing this interface will be
- *  automatically discovered when placed in the configured location on the classpath.</p>
+ * <p>Migration classes implementing this interface will be
+ * automatically discovered when placed in the configured location on the classpath.</p>
  *
- *  <p>Most users will be better served by subclassing subclass {@link BaseDataMigration} instead of implementing this
- *  interface directly, as {@link BaseDataMigration} encourages the use of Datalift's default naming convention and
- *  comes with helper methods for JSON-based Migrations.</p>
+ * <p>Most users will be better served by subclassing subclass {@link BaseDataMigration} instead of implementing this
+ * interface directly, as {@link BaseDataMigration} encourages the use of Datalift's default naming convention and comes with helper methods for JSON-based Migrations.</p>
  */
 public interface DataMigration {
 
   /**
-   *
    * @return The version of this data migration. Migrations are executed in ascending order of their versions.
    */
   int version();
